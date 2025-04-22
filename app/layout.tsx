9899372b84f6e252a6ba7a2f51044c2e6c608bbe
talhaz007 <talhazafar0007@gmail.com>
2025-04-23 00:08:02 +0500

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Dynamic Star Construction',
+  description: 'Professional renovation services that transform your property with expert craftsmanship and premium materials. Get a Free Estimate',
+  generator: 'Dyn',
 }
 
 export default function RootLayout({
